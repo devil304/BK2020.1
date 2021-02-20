bd7ff2059ece55ac98f00bfa3da8c4c1711e833a
Szymon Rzosinski <szymonrzosinski@gmail.com>
2021-02-20 20:16:42 +0100

    Yes

--- Assets/Scripts/my/blockFinish.cs
+++ Assets/Scripts/my/blockFinish.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class blockFinish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    BoxCollider myBC;
+    Quest[] quests;
+    [SerializeField] TextMeshProUGUI tmpT;
+    [SerializeField] string text;
+    private void Start()
     {
-        
+        myBC = GetComponent<BoxCollider>();
+        quests = FindObjectsOfType<Quest>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        bool tmp = true;
+        foreach(Quest q in quests)
+        {
+            if (!q.done)
+            {
+                tmp = false;
+                break;
+            }
+        }
+        if (tmp)
+            myBC.enabled = false;
+        else
+        {
+            tmpT.text = text;
+            tmpT.enabled = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionExit(Collision collision)
     {
-        
+        tmpT.enabled = false;
     }
 }
